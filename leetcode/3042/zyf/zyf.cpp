c91e13f4607be338d1f7f5c9e2ca01b6a5a9f9d0
qumingzizhenmafan <142280594+qumingzizhenmafan@users.noreply.github.com>
2024-02-20 15:09:19 +0800

    24.2.20.3

---
+++ leetcode/3042/zyf/zyf.cpp
@@ -0,0 +1,27 @@
+#include "../../zyfziyongtou.h"
+
+class Solution {
+    int isPrefixAndSuffix(string a, string b){
+        size_t na = a.size();
+        size_t nb = b.size();
+        if(na > nb) return 0;
+        if(a == b.substr(0,na) && a == b.substr(nb - na, na)) return 1;
+        else return 0;
+    }
+public:
+    int countPrefixSuffixPairs(vector<string>& words) {
+        unordered_map<char, vector<string>> dp;
+        for(string s : words){
+            dp[s[0]].push_back(s);
+        }
+        int target = 0;
+        for(auto& [c,v] : dp){
+            size_t n = v.size();
+            for(int i = 0; i < n - 1; ++i)
+                for(int j = i + 1; j < n; ++j){
+                    target += isPrefixAndSuffix(v[i] , v[j]);
+                }
+        }
+        return target;
+    }
+};
